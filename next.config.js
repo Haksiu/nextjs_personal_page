4284cfe67bb67a1b93b738bb000c0f057fa20da8
Haksiu <38468996+Haksiu@users.noreply.github.com>
2022-08-16 00:00:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: "imgix",
-    path: "",
-  },
+  images.unoptimized = true
 }
 
 module.exports = nextConfig
