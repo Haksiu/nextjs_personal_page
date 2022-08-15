132540a7b0effcf84963ed479ec2dbfe03f1ee49
Haksiu <38468996+Haksiu@users.noreply.github.com>
2022-08-15 23:38:04 +0200

    fix

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import {FaLinkedinIn, FaDiscord} from 'react-icons/fa'
-import {AiOutlineMail} from 'react-icons/ai'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import React from "react"
+import {FaLinkedinIn, FaDiscord} from "react-icons/fa"
+import {AiOutlineMail} from "react-icons/ai"
+import {BsFillPersonLinesFill} from "react-icons/bs"
 
 const Contact = () => {
   return (
-    <div id="contact" className='w-full lg:h-screen'>
-        <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
-        <p className='text-xl tracking-widest uppercase text-[#37b25a]'>Contact</p>
-        <h2 className='py-4'>Contact</h2>
-        <div className='grid lg:grid-cols-5 gap-8'>
+    <div id="contact" className="w-full lg:h-screen">
+        <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase text-[#37b25a]">Contact</p>
+        <h2 className="py-4">Contact</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
             {/* left */}
-            <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-300 rounded-xl p-4'>
-                <div className='lg:p-4 h-full'>
+            <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-300 rounded-xl p-4">
+                <div className="lg:p-4 h-full">
                     <div>
-                        <h2 className='py-2'>Haksiu</h2>
+                        <h2 className="py-2">Haksiu</h2>
                         <p>Beginner Full-Stack Developer</p>
-                        <p className='py-4'>I am avaible for freelance position. Contact with me let's talk.</p>
+                        <p className="py-4">I am avaible for freelance position. Contact with me let&apos;s talk.</p>
                     </div>
                     <div>
-                    <p className='uppercase pt-8'>Connect With Me:</p>
-                    <div className='flex items-center justify-between'>
-                        <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300'>
+                    <p className="uppercase pt-8">Connect With Me:</p>
+                    <div className="flex items-center justify-between">
+                        <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300">
                             <FaDiscord />
                         </div>
-                        <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300'>
+                        <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300">
                             <AiOutlineMail />
                         </div>
-                        <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300'>
+                        <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300">
                             <FaLinkedinIn />
                         </div>
-                        <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300'>
+                        <div className="rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 easy-in duration-300">
                             <BsFillPersonLinesFill />
                         </div>
                     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react'
-import Link from 'next/link'
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import React, {useEffect, useState} from "react"
+import Link from "next/link"
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -19,7 +19,7 @@ useEffect(() => {
       setShadow(false);
     }
   }
-  window.addEventListener('scroll', handleShadow);
+  window.addEventListener("scroll", handleShadow);
 }, [])
 
   return (
@@ -27,55 +27,55 @@ useEffect(() => {
         <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
           LOGO PLACEHOLDER
           <div>
-              <ul className='hidden md:flex'>
+              <ul className="hidden md:flex">
                 <Link href="/">
-                    <li className='ml-10 text-sm uppercase font-bold text-gray-700'>Home</li>
+                    <li className="ml-10 text-sm uppercase font-bold text-gray-700">Home</li>
                 </Link>
                 <Link href="/#about">
-                    <li className='ml-10 text-sm uppercase font-bold text-gray-700'>About</li>
+                    <li className="ml-10 text-sm uppercase font-bold text-gray-700">About</li>
                 </Link>
                 <Link href="/#skills">
-                    <li className='ml-10 text-sm uppercase font-bold text-gray-700'>Skills</li>
+                    <li className="ml-10 text-sm uppercase font-bold text-gray-700">Skills</li>
                 </Link>
                 <Link href="/#contact">
-                    <li className='ml-10 text-sm uppercase font-bold text-gray-700'>Contact</li>
+                    <li className="ml-10 text-sm uppercase font-bold text-gray-700">Contact</li>
                 </Link>
               </ul>
-              <div onClick={handleNav} className='md:hidden' >
+              <div onClick={handleNav} className="md:hidden" >
                 <AiOutlineMenu size={25}/>
               </div>
           </div>
         </div>
         <div 
-        className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}
+        className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}
           >
-          <div className={nav ? 'md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#edeff2] p-10 easy-in duration-500' : 
-        'fixed left-[-100%] top-0 p-10 easy-in duration-500'}>
+          <div className={nav ? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#edeff2] p-10 easy-in duration-500" : 
+        "fixed left-[-100%] top-0 p-10 easy-in duration-500"}>
             <div>
-              <div className='flex justify-between items-center w-full'>
+              <div className="flex justify-between items-center w-full">
                   LOGO PLACEHOLDER
-                  <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                  <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
                     <AiOutlineClose />
                   </div>
               </div>
-              <div className='border-b border-gray-300 my-4 '>
-                <p className='w-[85%] md:w-[90%] py-4'>This is my dream portfolio website</p>
+              <div className="border-b border-gray-300 my-4 ">
+                <p className="w-[85%] md:w-[90%] py-4">This is my dream portfolio website</p>
               </div>
             </div>
 
-            <div className='py-4 flex flex-col'>
-              <ul className='uppercase'>
+            <div className="py-4 flex flex-col">
+              <ul className="uppercase">
                 <Link href="/" >
-                  <li onClick={() => setNav(false)} className='py-4 text-sm'>Home</li>
+                  <li onClick={() => setNav(false)} className="py-4 text-sm">Home</li>
                 </Link>
                 <Link href="/#about" >
-                  <li onClick={() => setNav(false)} className='py-4 text-sm'>About</li>
+                  <li onClick={() => setNav(false)} className="py-4 text-sm">About</li>
                 </Link>
                 <Link href="/#skills" >
-                  <li onClick={() => setNav(false)} className='py-4 text-sm'>Skills</li>
+                  <li onClick={() => setNav(false)} className="py-4 text-sm">Skills</li>
                 </Link>
                 <Link href="/#contact" >
-                  <li onClick={() => setNav(false)} className='py-4 text-sm'>Contact</li>
+                  <li onClick={() => setNav(false)} className="py-4 text-sm">Contact</li>
                 </Link>
               </ul>
             </div>
