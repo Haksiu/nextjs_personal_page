bc5550b83ba669e4059a4285c4e2b62867da1cc9
Haksiu <38468996+Haksiu@users.noreply.github.com>
2022-08-22 00:10:01 +0200

    update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return ( 
         <div className='flex w-full h-20 bg-[#dfe2e6] items-center justify-center'>
         <p >
-           &copy; Haksiu 2022- {new Date().getFullYear()}
+           &copy; Haksiu 2022 - {new Date().getFullYear()}
         </p>
         </div>
     )
