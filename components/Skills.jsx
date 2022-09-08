252f0e6517edc75d1ce63a9bd0abe3331a846f54
Haksiu <38468996+Haksiu@users.noreply.github.com>
2022-09-08 13:38:07 +0200

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -61,16 +61,6 @@ const Skills = () => {
             <div className="p-6 shadow-xl rounded-xl hover:scale-105 easy-in duration-300">
                 <div className="grid grid-cols-2 gap-4 justify-center items-center">
                     <div className="m-auto">
-                        <Image src="/assets/php.png" width="64px" height="64px"  alt="/" unoptimized={true}/>
-                    </div>
-                    <div className="flex flex-col items-center justify-center">
-                        <h3>PHP</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="p-6 shadow-xl rounded-xl hover:scale-105 easy-in duration-300">
-                <div className="grid grid-cols-2 gap-4 justify-center items-center">
-                    <div className="m-auto">
                         <Image src="/assets/react.png" width="64px" height="64px"  alt="/" unoptimized={true}/>
                     </div>
                     <div className="flex flex-col items-center justify-center">
